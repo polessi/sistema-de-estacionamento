a2e9c2652894abd5f01c354100814fe5e8390e3b
polessi <polessidev@gmail.com>
2023-11-15 13:09:58 -0300

    Adicionando o objeto responsável pelas funções que serão utilizadas no projeto.

--- script.ts
+++ script.ts
@@ -2,6 +2,21 @@
     const $ = (query: string): HTMLInputElement | null => 
     document.querySelector(query);
 
+    function patio(){
+        function ler(){}
+
+        function adicionar(){}
+
+        function remover(){}
+
+        function salvar(){}
+
+        function render(){}
+
+        return { ler, adicionar, remover, salvar, render }
+
+    }
+
     $("#cadastrar")?.addEventListener("click", () => {
         console.log('aqui');
         const nome = $("#nome")?.value;
